ae8b92b12a35eb768b8b8c93f9901e73951dc116
Sahar <sahar.sadrii.ss@gmail.com>
2019-12-25 13:48:21 +0330

    Add two variable to database

--- Database/MYSQL Practice Class.sql
+++ Database/MYSQL Practice Class.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 24, 2019 at 10:11 PM
+-- Generation Time: Dec 25, 2019 at 11:08 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
